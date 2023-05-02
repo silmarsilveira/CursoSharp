ddea74910341c473f4c1e3d326b90e55ceb916d2
LBVDC\silmarl <silmarl@lbv.org.br>
2023-05-02 09:47:42 -0300

    Meu curso

---
+++ Course/Entities/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Course.Entities.Enums;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Entities
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return Id
+                + ","
+                + Moment
+                + ","
+                + Status;
+        }
+
+    }
+}
---
+++ Course/Program.cs
@@ -0,0 +1,30 @@
+﻿using Course.Entities;
+using Course.Entities.Enums;
+using System;
+
+namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = Entities.Enums.OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine( order );
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            Console.WriteLine( txt);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine( os );
+
+        }
+    }
+}
